3fee5d02a359e67b1f940ae2eae2c9b3c13ea71d
DrRudi <dr.rudi2@gmail.com>
2019-03-11 19:56:47 +0300

    Emotion spinner test

--- app/src/main/java/com/app/cbtrack/FeelingsSelection.kt
+++ app/src/main/java/com/app/cbtrack/FeelingsSelection.kt
@@ -4,6 +4,10 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.Button
+import android.R.attr.data
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+
 
 class FeelingsSelection : AppCompatActivity() {
 
@@ -11,6 +15,15 @@ class FeelingsSelection : AppCompatActivity() {
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.emotion_selection)
+
+        val data_anger = arrayOf("nothing", "two", "three", "four", "five")
+        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, data_anger)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        val spinnerAnger = findViewById<Spinner>(R.id.spinner_anger)
+        spinnerAnger.setAdapter(adapter)
+
+        spinnerAnger.setSelection(0)
 
         val buttonSave = findViewById<Button>(R.id.button_save_emotions)
         val buttonBack = findViewById<Button>(R.id.button_back_emotion_selection)
