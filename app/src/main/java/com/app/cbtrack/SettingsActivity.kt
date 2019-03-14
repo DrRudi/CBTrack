fceb77e7194fd4b5b097fb91f29015eb1d87ea2a
Shokk357 <pavcygan17@gmail.com>
2019-03-14 22:59:40 +0300

    add Info and Settings activity.

---
+++ app/src/main/java/com/app/cbtrack/SettingsActivity.kt
@@ -0,0 +1,26 @@
+package com.app.cbtrack
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import android.widget.Switch
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+
+        val description = (R.id.description)
+        val recordsAccess = findViewById<Switch>(R.id.records_access)
+        val apartPgAcs = (R.id.apart_page_access)
+        val descriptionAptPg = (R.id.description_apart_page)
+        val shareAcs = (R.id.share_access)
+        val mailWndw = (R.id.mail_window)
+
+        val buttonShr = findViewById<Button>(R.id.button_share)
+        buttonShr.setOnClickListener {
+//            TODO
+        }
+
+    }
+}
