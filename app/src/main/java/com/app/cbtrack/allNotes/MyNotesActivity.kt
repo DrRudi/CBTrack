2fb060ccc6f1135d27131be70f0fd213f4c95bb4
Sasha <sashasashasasha151@gmail.com>
2019-03-15 22:11:56 +0300

    my notes activity design

--- app/src/main/java/com/app/cbtrack/allNotes/MyNotesActivity.kt
+++ app/src/main/java/com/app/cbtrack/allNotes/MyNotesActivity.kt
@@ -1,38 +1,52 @@
 package com.app.cbtrack.allNotes
 
-import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
 import android.widget.Button
+import android.widget.TextView
 import com.app.cbtrack.R
 
 
-@SuppressLint("Registered")
 class MyNotesActivity : AppCompatActivity() {
+
+    private lateinit var emotionNotesButton: Button
+    private lateinit var thoughtNotesButton: Button
+    private lateinit var tagsNotesButton: Button
+    private lateinit var allNotesButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_notes)
 
-        val buttonThoughts = findViewById<Button>(R.id.thoughts_notes_button)
-        buttonThoughts.setOnClickListener {
+        val toolbar = findViewById<Toolbar>(R.id.my_toolbar)
+        val mTitle = toolbar.findViewById<TextView>(R.id.toolbar_title)
+
+        setSupportActionBar(toolbar)
+        mTitle.text = resources.getString(R.string.my_notes);
+
+        supportActionBar!!.setDisplayShowTitleEnabled(false)
+
+        emotionNotesButton = findViewById(R.id.emotion_notes_button)
+        thoughtNotesButton = findViewById(R.id.thoughts_notes_button)
+        tagsNotesButton = findViewById(R.id.tags_notes_button)
+        allNotesButton = findViewById(R.id.all_notes_button)
+
+        thoughtNotesButton.setOnClickListener {
             //TODO: go to my_thoughts
         }
 
-        val buttonEmotion = findViewById<Button>(R.id.emotion_notes_button)
-        buttonEmotion.setOnClickListener {
+        emotionNotesButton.setOnClickListener {
             //TODO: go to emotions
         }
 
-        val buttonAllnotes = findViewById<Button>(R.id.all_notes_button)
-        buttonAllnotes.setOnClickListener {
+        allNotesButton.setOnClickListener {
             val intent = Intent(this@MyNotesActivity, AllNotesActivity::class.java)
             startActivity(intent)
         }
 
-        val buttonTags = findViewById<Button>(R.id.tags_button)
-        buttonTags.setOnClickListener {
+        tagsNotesButton.setOnClickListener {
             val intent = Intent(this@MyNotesActivity, AActivity::class.java)
             startActivity(intent)
         }
