602fbebd0f440429a171ca878371f7641f2bb57f
Sasha <sashasashasasha151@gmail.com>
2019-03-15 21:29:14 +0300

    main activity desingn

--- app/src/main/java/com/app/cbtrack/main/MainActivity.kt
+++ app/src/main/java/com/app/cbtrack/main/MainActivity.kt
@@ -1,37 +1,65 @@
 package com.app.cbtrack.main
 
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
 import android.widget.Button
+import android.widget.TextView
+import com.app.cbtrack.R
 import com.app.cbtrack.addNotes.AddEmotionActivity
 import com.app.cbtrack.addNotes.AddThoughtActivity
 import com.app.cbtrack.allNotes.MyNotesActivity
-import com.app.cbtrack.R
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var infoButton: Button
+    private lateinit var settingsButton: Button
+    private lateinit var addThoughtButton: Button
+    private lateinit var addEmotionButton: Button
+    private lateinit var allNotesButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val button = findViewById<Button>(R.id.all_content_b)
-        button.setOnClickListener {
-            val intent = Intent(this@MainActivity, MyNotesActivity::class.java)
-            startActivity(intent)
+        val toolbar = findViewById<Toolbar>(R.id.my_toolbar)
+        val mTitle = toolbar.findViewById<TextView>(R.id.toolbar_title)
+
+        setSupportActionBar(toolbar)
+        mTitle.text = toolbar.title
+
+        supportActionBar!!.setDisplayShowTitleEnabled(false)
+
+        infoButton = toolbar.findViewById(R.id.info_b)
+        settingsButton = toolbar.findViewById(R.id.settings_b)
+        addThoughtButton = findViewById(R.id.add_thoughts_b)
+        addEmotionButton = findViewById(R.id.add_emotion_b)
+        allNotesButton = findViewById(R.id.all_notes_b)
+
+        infoButton.setOnClickListener {
+            //TODO
         }
 
-        val button2 = findViewById<Button>(R.id.add_thoughts_b)
-        button2.setOnClickListener {
+        settingsButton.setOnClickListener {
+            //TODO
+        }
+
+
+        addThoughtButton.setOnClickListener {
             val intent = Intent(this@MainActivity, AddThoughtActivity::class.java)
             startActivity(intent)
         }
 
-        val button3 = findViewById<Button>(R.id.add_feelings_b)
-        button3.setOnClickListener {
+        addEmotionButton.setOnClickListener {
             val intent = Intent(this@MainActivity, AddEmotionActivity::class.java)
             startActivity(intent)
         }
 
+        allNotesButton.setOnClickListener {
+            val intent = Intent(this@MainActivity, MyNotesActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
